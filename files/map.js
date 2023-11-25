259751ed7269a11d65a30a35000a74fea6ad3bbd
HideOnCity <3244529406@qq.com>
2023-11-25 20:18:06 +0800

    init

---
+++ files/map.js
@@ -0,0 +1,154 @@
+const WEB_SITE_MAP = [
+    {
+        name: '超级导航A',
+        sites: [
+            {
+                name: '网址A',
+                address: 'https://www.baidu.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            }
+        ]
+    },
+    {
+        name: '超级导航B',
+        sites: [
+            {
+                name: '网址A',
+                address: 'https://www.baidu.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            }
+        ]
+    },
+    {
+        name: '超级导航C',
+        sites: [
+            {
+                name: '网址A',
+                address: 'https://www.baidu.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            }
+        ]
+    },
+    {
+        name: '超级导航D',
+        sites: [
+            {
+                name: '网址A',
+                address: 'https://www.baidu.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            },
+            {
+                name: '网址B',
+                address: 'https://www.hao123.com'
+            }
+        ]
+    }
+]
